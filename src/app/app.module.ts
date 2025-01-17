c306b235b81387cbe5706f6cdf809e04f88d026b
antonio-bit-1109 <antonio.rizzuti@hotmail.com>
2025-01-17 12:07:24 +0100

    inizio inserimento validatori registrazione START

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
+// moduli
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { AppRoutingModule } from './app-routing.module';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+//componenti
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistrazioneComponent } from './components/registrazione/registrazione.component';
-import { RadioButtonModule } from 'primeng/radiobutton';
+
 @NgModule({
   declarations: [AppComponent, LoginComponent, RegistrazioneComponent],
   imports: [
@@ -18,6 +22,8 @@ import { RadioButtonModule } from 'primeng/radiobutton';
     BrowserAnimationsModule,
     RouterModule,
     RadioButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/registrazione/registrazione.component.ts
+++ src/app/components/registrazione/registrazione.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrazione',
   standalone: false,
-  
+
   templateUrl: './registrazione.component.html',
-  styleUrl: './registrazione.component.scss'
+  styleUrl: './registrazione.component.scss',
 })
 export class RegistrazioneComponent {
+  public form = new FormGroup({
+    nome: new FormControl('', [Validators.required]),
+    cognome: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    ripetiPassword: new FormControl('', [Validators.required]),
+    checkUomo: new FormControl(false, []),
+    checkDonna: new FormControl(false, []),
+    accetto: new FormControl('', [Validators.required]),
+  });
 
+  public onSubmit() {
+    console.log(this.form.value);
+  }
 }
